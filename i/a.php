ebbb167e0787d7d17d33c65c65c53a9a3053d85f
14101-pearvv <66010914101@msu.ac.th>
2026-02-10 09:58:00 +0700

    Add files via upload

---
+++ i/a.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>กุลปริยา แก้วตา (แพรว)</title>
+</head>
+
+<body>
+
+<h1>งาน i</h1>
+<h1>กุลปริยา แก้วตา (แพรว)</h1>
+
+</body>
+</html>
